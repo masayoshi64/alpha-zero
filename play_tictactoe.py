a232b2828be8ab82664173194e3932a0625fdb19
masayoshi64 <sho32001025@gmail.com>
2022-09-16 22:31:13 +0900

    相手プレイヤーを指定できるように

--- play_tictactoe.py
+++ play_tictactoe.py
@@ -1,12 +1,29 @@
-from games.arena import Arena
-from games.tictactoe import TicTacToeGame
-from games.players import RandomPlayer, HumanPlayer
+import argparse
+
+from app.games.arena import Arena
+from app.games.tictactoe import TicTacToeGame
+from app.games.players import RandomPlayer, HumanPlayer, MCTSPlayer
+from app.alpha_zero.nnet import ConstantNet
+from app.alpha_zero.mcts import MCTS
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("type")
+    args = parser.parse_args()
+    player_type = args.type
+
     game = TicTacToeGame(3)
     player1 = HumanPlayer(game)
-    player2 = RandomPlayer(game)
+    if player_type == "random":
+        player2 = RandomPlayer(game)
+    elif player_type == "mcts":
+        net = ConstantNet(game)
+        mcts = MCTS(game, net, 0.1)
+        player2 = MCTSPlayer(mcts, 1000)
+    else:
+        raise ValueError(f"Invalid player type: {player_type}")
+
     arena = Arena(player1.play, player2.play, game)
     arena.play_game(verbose=1)
 
