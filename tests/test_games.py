9f8f39ae051dc1a9c8298660a5b98207525c23a0
masayoshi64 <sho32001025@gmail.com>
2022-09-19 22:27:03 +0900

    リファクタリング

--- tests/test_games.py
+++ tests/test_games.py
@@ -8,7 +8,6 @@ from app.games.players import RandomPlayer
 
 
 def test_tictactoe():
-    print("hello")
     game = TicTacToeGame(3)
     player1, player2 = RandomPlayer(game), RandomPlayer(game)
     arena = Arena(player1.play, player2.play, game)
