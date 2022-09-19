d41b5509912dc44560bac6c3dfaf90b73be1bde8
masayoshi64 <sho32001025@gmail.com>
2022-09-19 22:41:27 +0900

    リバーシを実装

--- tests/test_games.py
+++ tests/test_games.py
@@ -1,4 +1,5 @@
 from app.games.tictactoe import TicTacToeGame
+from app.games.reversi import ReversiGame
 from app.games.arena import Arena
 from app.games.players import RandomPlayer
 
@@ -9,6 +10,13 @@ from app.games.players import RandomPlayer
 
 def test_tictactoe():
     game = TicTacToeGame(3)
+    player1, player2 = RandomPlayer(game), RandomPlayer(game)
+    arena = Arena(player1.play, player2.play, game)
+    arena.play_game(verbose=1)
+
+
+def test_reversi():
+    game = ReversiGame(6)
     player1, player2 = RandomPlayer(game), RandomPlayer(game)
     arena = Arena(player1.play, player2.play, game)
     arena.play_game(verbose=1)
