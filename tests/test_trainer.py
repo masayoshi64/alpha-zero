2101212358478b6dd915eced391299fe83cc3937
masayoshi64 <sho32001025@gmail.com>
2022-09-20 09:17:48 +0900

    bug fix

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -5,7 +5,7 @@ from app.games.tictactoe import TicTacToeGame
 
 def test_trainer():
     game = TicTacToeGame(3)
-    trainer = Trainer(game, 20, 10, 10, 50, 0, 0.01, 0.1, 1.0, 10)
+    trainer = Trainer(game, 20, 10, 10, 50, 0.01, 0, 0.1, 1.0, 10)
     model = OneLayerModel(game)
     model = trainer.train(model)
     cmodel = ConstantModel(game)
