794a9c60a516ae898abb584455b8fb517d1d2310
masayoshi64 <sho32001025@gmail.com>
2022-09-20 09:12:59 +0900

    先手と後手両方で評価

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,9 +1,6 @@
 from app.alpha_zero.trainer import Trainer
 from app.alpha_zero.models import OneLayerModel, ConstantModel
-from app.alpha_zero.mcts import MCTS
-from app.games.arena import Arena
 from app.games.tictactoe import TicTacToeGame
-from app.games.players import MCTSPlayer
 
 
 def test_trainer():
@@ -11,11 +8,6 @@ def test_trainer():
     trainer = Trainer(game, 20, 10, 10, 50, 0, 0.01, 0.1, 1.0, 10)
     model = OneLayerModel(game)
     model = trainer.train(model)
-    mcts = MCTS(game, model, 0.1, 1, 10)
-    cmcts = MCTS(game, ConstantModel(game), 0.1, 1, 10)
-    player1 = MCTSPlayer(mcts)
-    player2 = MCTSPlayer(cmcts)
-    arena = Arena(player1.play, player2.play, game)
-    r = arena.play_games(50)
-    # 勝率9割以上
+    cmodel = ConstantModel(game)
+    r = trainer.eval(model, cmodel)
     assert r > 0.5
